081800f398806eeffc720413cc080429f376092c
Brant Wynn <brant.wynn@acquia.com>
2020-02-10 16:57:08 -0600

    Moves the robo file, get rid of lightning-dev usage keeping it as dep.

--- RoboFile.php
+++ RoboFile.php
@@ -1,5 +1,9 @@
 <?php
 
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Yaml\Yaml;
+use Drupal\Component\Utility\NestedArray;
+
 class RoboFile extends \Robo\Tasks {
 
   /**
@@ -24,6 +28,46 @@ class RoboFile extends \Robo\Tasks {
     return $this->taskExec($drush ?: '../vendor/bin/drush')
       ->rawArg($command)
       ->dir('docroot');
+  }
+
+  /**
+   * Configures Behat.
+   *
+   * @param string $base_url
+   *  (optional) The URL of the Drupal site.
+   *
+   * @return \Robo\Contract\TaskInterface
+   *   The task to execute.
+   */
+  public function configureBehat ($base_url = NULL) {
+    $configuration = [];
+
+    /** @var Finder $partials */
+    $partials = Finder::create()->in('docroot')->files()->name('behat.partial.yml');
+
+    foreach ($partials as $partial) {
+      $partial = str_replace(
+        '%paths.base%',
+        $partial->getPathInfo()->getRealPath(),
+        $partial->getContents()
+      );
+      $configuration = NestedArray::mergeDeep($configuration, Yaml::parse($partial));
+    }
+
+    if ($configuration) {
+      $configuration = str_replace([
+          '%base_url%',
+          '%drupal_root%',
+        ],
+        [
+          $base_url ?: static::BASE_URL,
+          'docroot',
+        ],
+        Yaml::dump($configuration)
+      );
+      return $this->taskWriteToFile('.behat.yml')->text($configuration);
+    }
+
   }
 
   /**
